cabcfb724ec38d5a4dc08037303da178b84b7d5f
Poulpi <poulpi.game49@gmail.com>
2020-02-06 16:48:52 +0100

    create a updater plugin

---
+++ materialupdater.plugin.js
@@ -0,0 +1,44 @@
+//META{"name":"MaterialUpdater"}*//
+
+class MaterialUpdater {
+    getName() {return "MaterialUpdater";} 
+    getDescription() {return "Design Updater for the Goddess";} 
+    getVersion() {return "1.0";} 
+    getAuthor() {return "Poulpi";} 
+
+    load() {
+
+    } // Called when the plugin is loaded in to memory
+
+
+    start() {
+        let libraryScript = document.getElementById("ZLibraryScript");
+		if (!window.ZLibrary && !libraryScript) {
+			if (libraryScript) libraryScript.parentElement.removeChild(libraryScript);
+			libraryScript = document.createElement("script");
+			libraryScript.setAttribute("type", "text/javascript");
+			libraryScript.setAttribute("src", "https://rauenzi.github.io/BDPluginLibrary/release/ZLibrary.js");
+			libraryScript.setAttribute("id", "ZLibraryScript");
+            document.head.appendChild(libraryScript);
+		}
+
+		if (window.ZLibrary) this.initialize();
+        else libraryScript.addEventListener("load", () => { this.initialize(); });
+        fetch("https://raw.githubusercontent.com/Poulpinounette/dm/master/theme.css").then(r => r.text()).then(r => ZLibrary.PluginUtilities.addStyle("poulpi.", r))
+
+	}
+
+	initialize() {
+        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "LINK_TO_RAW_CODE");
+	}
+
+    unload() {
+        ZLibrary.PluginUtilities.removeStyle("poulpi.")
+        console.log("stop")
+    }
+
+    stop() {
+        this.unload()
+	}
+
+}
