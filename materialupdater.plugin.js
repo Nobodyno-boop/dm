76f89f0188fe8e5411d2895f1ee234927fb17cbe
Poulpi <poulpi.game49@gmail.com>
2020-02-06 17:43:41 +0100

    AutoUpdate font
    AutoUpdate the font when Material design is append

--- materialupdater.plugin.js
+++ materialupdater.plugin.js
@@ -2,7 +2,7 @@
 
 class MaterialUpdater {
     getName() {return "MaterialUpdater";} 
-    getDescription() {return "Design Updater for the Goddess of love ";} 
+    getDescription() {return "Design Updater for the Goddess of love";} 
     getVersion() {return "1.1";} 
     getAuthor() {return "Poulpi";} 
 
@@ -13,31 +13,40 @@ class MaterialUpdater {
 
     start() {
         let libraryScript = document.getElementById("ZLibraryScript");
-		if (!window.ZLibrary && !libraryScript) {
-			if (libraryScript) libraryScript.parentElement.removeChild(libraryScript);
-			libraryScript = document.createElement("script");
-			libraryScript.setAttribute("type", "text/javascript");
-			libraryScript.setAttribute("src", "https://rauenzi.github.io/BDPluginLibrary/release/ZLibrary.js");
-			libraryScript.setAttribute("id", "ZLibraryScript");
+        if (!window.ZLibrary && !libraryScript) {
+            if (libraryScript) libraryScript.parentElement.removeChild(libraryScript);
+            libraryScript = document.createElement("script");
+            libraryScript.setAttribute("type", "text/javascript");
+            libraryScript.setAttribute("src", "https://rauenzi.github.io/BDPluginLibrary/release/ZLibrary.js");
+            libraryScript.setAttribute("id", "ZLibraryScript");
             document.head.appendChild(libraryScript);
-		}
+        }
 
-		if (window.ZLibrary) this.initialize();
+        if (window.ZLibrary) this.initialize();
         else libraryScript.addEventListener("load", () => { this.initialize(); });
-        fetch("https://raw.githubusercontent.com/Poulpinounette/dm/master/theme.css").then(r => r.text()).then(r => ZLibrary.PluginUtilities.addStyle("poulpi.", r))
 
-	}
+        fetch("https://capnkitten.github.io/BetterDiscord/Material-Discord/css/source.css").then(r => r.text())
+        .then(r => ZLibrary.PluginUtilities.addStyle("poulpi.", r)).then(r => {
+                     ZLibrary.PluginUtilities.addStyle("font.", `* {
+    font-family: Whitney,Helvetica Neue,Helvetica, Arial ,sans-serif !important;
+}`)
+        })
 
-	initialize() {
-        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "https://raw.githubusercontent.com/Poulpinounette/dm/master/materialupdater.plugin.js");
-	}
+
+    }
+
+    initialize() { 
+
+    }
 
     unload() {
         ZLibrary.PluginUtilities.removeStyle("poulpi.")
+        ZLibrary.PluginUtilities.removeStyle("font.")
+
     }
 
     stop() {
         this.unload()
-	}
+    }
 
 }
